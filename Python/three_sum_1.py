c97e6441b0589ae1aa6a046fab44df55eeb83cd9
zhangwei <zhangwei@cinehello.com>
2018-12-16 11:51:53 +0800

    Python3 - three sum

---
+++ Python/three_sum_1.py
@@ -0,0 +1,22 @@
+# Difficulty - Medium
+# Url - https://leetcode.com/problems/3sum/
+# Time complexity: O(n2);
+# Space complexity: O(n);
+
+class Solution:
+    def threeSum(self, nums):
+        if len(nums) < 3:
+            return []
+        nums.sort()
+        res = set()
+        for i, v in enumerate(nums[:-2]):
+            if i >= 1 and v == nums[i-1]:
+                continue
+            d = {}
+            for x in nums[i+1:]:
+                if x not in d:
+                    d[-v-x] = 1
+                else:
+                    res.add((v, -v-x, x))
+
+        return list(res)
