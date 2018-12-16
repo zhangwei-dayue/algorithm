c97555f8344346de7e15b7a37a1307f935d46bad
zhangwei <zhangwei19890518@gmail.com>
2018-12-16 12:03:56 +0800

    Python3 - sliding_window_maximum

--- Python/sliding_window_maximum_python.py
+++ Python/sliding_window_maximum_python.py
@@ -1,2 +1,19 @@
+# Difficulty - Hard
+# Url - https://leetcode.com/problems/sliding-window-maximum/
+# Time complexity: O(n);
+# Space complexity: O(1);
+
 class Solution:
     def maxSlidingWindow(self, nums, k):
+        if not nums: return []
+        window, res = [], []
+        for i, x in enumerate(nums):
+            if i >= k and window[0] <= i - k:
+                window.pop(0)
+            while window and nums[window[-1]] <= x:
+                window.pop()
+            window.append(i)
+            if i >= k - 1:
+                res.append(nums[window[0]])
+
+        return res
